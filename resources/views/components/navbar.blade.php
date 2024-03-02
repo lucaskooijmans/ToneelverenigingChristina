aeafcd08d76ba9839007e0329cb959eaf8ce9129
Thomas Versteeg <71022949+JathoSystems@users.noreply.github.com>
2024-03-02 15:52:01 +0100

    Look & Feel Navigatiebalk
    1e opzet van de navigatiebalk. Voor nu gebruik ik rood als de accentkleur. Nu moeten de links nog via de named links in het Routes bestand

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,12 @@
+<nav>
+    <h1 class="brand">Toneelvereniging<br>Christina<br>Ravenswaaij</h1>
+    <div class="links">
+        <a href="/">Home</a>
+        <a href="/voorstellingen">Voorstellingen</a>
+        <a href="/nieuws">Nieuws</a>
+        <a href="/historie">Historie</a>
+        <a href="/gallerij">Gallerij</a>
+        <a href="/contact">Contact</a>
+        <a href="/registreren">Lid Worden</a>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('uitesting', function () {
+    return view('uitesting');
+});
