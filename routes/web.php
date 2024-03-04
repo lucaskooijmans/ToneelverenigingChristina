4eeb05aa347eec4f67c5d458ff3f1c0af8696a2d
lucas <lam.kooijmans@student.avans.nl>
2024-03-04 16:42:07 +0100

    Add beginning

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\PostController;
+    use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/nieuws', [PostController::class, 'index']);
+Route::resource('posts', PostController::class);
