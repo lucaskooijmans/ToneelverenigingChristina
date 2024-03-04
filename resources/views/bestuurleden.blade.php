61b0784b828c828aa9edd4a94780ebd0854294eb
KD <115158216+k68d@users.noreply.github.com>
2024-03-04 21:48:17 +0100

    Bestuursleden begin

---
+++ resources/views/bestuurleden.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bestuurleden</title>
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="grid-container">
+        <!-- Member 1 -->
+        <div class="member-card">
+            <div class="member-image">
+                <img src="https://2456764.fs1.hubspotusercontent-na1.net/hub/2456764/hubfs/YoungPeopleBusinessMeeting_1200x627.jpg" alt="Board Member">
+            </div>
+            <h3>Naam Lid 1</h3>
+            <p>Email: email1@example.com</p>
+            <p>Telefoonnummer: 0612345678</p>
+            <p>Beschrijving: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <!-- Member 2 -->
+        <div class="member-card">
+            <div class="member-image">
+                <img src="https://2456764.fs1.hubspotusercontent-na1.net/hub/2456764/hubfs/YoungPeopleBusinessMeeting_1200x627.jpg" alt="Board Member">
+            </div>
+            <h3>Naam Lid 2</h3>
+            <p>Email: email2@example.com</p>
+            <p>Telefoonnummer: 0612345678</p>
+            <p>Beschrijving: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <!-- Member 3 -->
+        <div class="member-card">
+            <div class="member-image">
+                <img src="https://2456764.fs1.hubspotusercontent-na1.net/hub/2456764/hubfs/YoungPeopleBusinessMeeting_1200x627.jpg" alt="Board Member">
+            </div>
+            <h3>Naam Lid 3</h3>
+            <p>Email: email3@example.com</p>
+            <p>Telefoonnummer: 0612345678</p>
+            <p>Beschrijving: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bestuursleden', function () {
+    return view('bestuurleden');
+});
