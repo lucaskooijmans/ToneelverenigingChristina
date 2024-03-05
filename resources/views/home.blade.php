d1b7293263b5bc87489e27b4acf95bbde9b457f6
Thomas Versteeg <71022949+JathoSystems@users.noreply.github.com>
2024-03-05 20:57:09 +0100

    Components + taal fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 
 <head>
     <meta charset="UTF-8">
@@ -28,68 +28,9 @@
         </div>
     </section>
 
-    <section class="aankomend">
-        <div class="container">
-            <h1>Titel van voorstelling</h1>
-            <div class="countdown">
-                <div class="countdown-item">
-                    <span id="days">12</span>
-                    <p>dagen</p>
-                </div>
-                <div class="countdown-item">
-                    <span id="hours">12</span>
-                    <p>uren</p>
-                </div>
-                <div class="countdown-item">
-                    <span id="minutes">12</span>
-                    <p>minuten</p>
-                </div>
-                <div class="countdown-item">
-                    <span id="seconds">12</span>
-                    <p>seconden</p>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-countdown />
 
-    <section class="aankondigingen">
-        <div class="container">
-            <h1>Aankondigingen</h1>
-            <div class="items">
-                <div class="aankondiging">
-                    <h1>Titel van aankondiging</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, blanditiis quos aperiam
-                            ratione illum nesciunt, dolores et temporibus exercitationem alias assumenda officia
-                            corrupti numquam aliquid nam ex obcaecati, atque voluptate a ut? Ex laudantium ab minus
-                            impedit ipsam quia consequatur itaque, soluta tempore praesentium obcaecati, reprehenderit
-                            exercitationem odio enim iure.
-                        </p>
-                </div>
-                <div class="aankondiging">
-                    <h1>Nog een titel van een aankondiging</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis fugiat ex quisquam
-                            voluptates distinctio at ea! Quae nemo eaque laboriosam voluptates quo, deserunt eligendi
-                            architecto, voluptatibus reprehenderit incidunt, tempore harum. At veritatis nesciunt error
-                            laudantium ratione molestiae possimus, consequuntur exercitationem iusto, rem tempore
-                            delectus quidem minus. Inventore, veniam minus sint molestiae aliquid quibusdam ullam quos
-                            ea voluptates cumque delectus debitis illo hic corporis alias fuga, necessitatibus,
-                            dignissimos ex blanditiis.
-                        </p>
-                </div>
-            </div>
-        </div>
-        <div class="meerzien">
-            <a href="/nieuws">
-                Meer aankondigingen
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                    stroke-width="2">
-                    <path d="M0 0 L10 10 L0 20" fill="none" stroke="black" stroke-width="2" />
-                </svg>
-            </a>
-        </div>
-    </section>
+    <x-aankondigingen />
 
 </body>
 
