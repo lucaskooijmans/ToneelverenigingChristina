52ccfd09c4d86c8f5949450d9a51f98372c91c34
Thoowmas <tkoerse@gmail.com>
2024-03-11 16:07:35 +0100

    Youtube embed.

--- resources/views/components/history_item.blade.php
+++ resources/views/components/history_item.blade.php
@@ -4,7 +4,7 @@
     <h2>{{ $historyItem->header }}</h2>
     <p>{{ $historyItem->optional_text_one }}</p>
     <img src="{{ asset('storage/' . $historyItem->image_path) }}" alt="" style="display: block; max-width: 100%; border-radius: 5px; margin-bottom: 10px;">
-
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/MYyJ4PuL4pY?rel=0" frameborder="0" allowfullscreen></iframe>
     <p>{{ $historyItem->optional_text_two }}</p>
     <h3>{{ $historyItem->optional_footer }}</h3>
     <p>{{ $historyItem->date }}</p>
