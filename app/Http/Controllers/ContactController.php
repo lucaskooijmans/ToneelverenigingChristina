77d22f8896925fb07c948316083ac21c12810ccb
Delluci <wmn.brunenberg@student.avans.nl>
2024-03-11 15:30:28 +0100

    wip(Contact): implement contact form

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function submit(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        $data = array(
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'subject' => $request->get('subject'),
+            'user_message' => $request->get('message'),
+        );
+        
+    
+        Mail::send('emails.contact', ['data' => $data], function($message) use ($data) {
+            $message->to(config('mail.from.address'))->subject('Contact Form Submission');
+            $message->from($data['email']);
+        });        
+    }
+}
