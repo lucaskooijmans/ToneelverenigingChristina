cf9b08a38c6965fec56780afc8b37a8537d870e4
Thomas Versteeg <71022949+JathoSystems@users.noreply.github.com>
2024-03-14 18:50:39 +0100

    Contact pagina styling v2
    Nu met de contact informatie

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -25,81 +25,62 @@
                 </div>
             @endif
 
-            <form method="POST" action="{{ route('contact.submit') }}" class="contact-form">
-                @csrf
-                <div class="form-group">
-                    <label for="name">Naam: <b>*</b></label>
-                    <input type="text" id="name" name="name" class="form-control" required>
-                </div>
-                <div class="form-group">
-                    <label for="email">Uw e-mailadres: <b>*</b></label>
-                    <input type="email" id="email" name="email" class="form-control" required>
-                </div>
-                <div class="form-group">
-                    <label for="subject">Onderwerp:</label>
-                    <input type="text" id="subject" name="subject" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="message">Bericht:</label>
-                    <textarea id="message" name="message" class="form-control"></textarea>
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="button green-button">Verstuur</button>
-                </div>
-                <label><b>*</b> Verplicht veld</label>
-            </form>
+            <div class="contact-split">
 
+                <form method="POST" action="{{ route('contact.submit') }}" class="contact-form">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name">Naam: <b>*</b></label>
+                        <input type="text" id="name" name="name" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Uw e-mailadres: <b>*</b></label>
+                        <input type="email" id="email" name="email" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="subject">Onderwerp:</label>
+                        <input type="text" id="subject" name="subject" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="message">Bericht:</label>
+                        <textarea id="message" name="message" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="button green-button">Verstuur</button>
+                    </div>
+                    <label><b>*</b> Verplicht veld</label>
+                </form>
+
+                <div class="contact-info">
+
+                    <div>
+                        <h1>Contactgegevens</h1></br>
+                        <label>Email</label></br>
+                        <label>toneelverenigingchristina@gmail.com</label>
+                    </div>
+                    <div>
+                        <label>Telefoonnummer<label></br>
+                        <label>06-36176711<label>
+                    </div>
+
+                    <div>
+                        <h1>FAQ</h1></br>
+                        <label>Vraag 1....</label></br>
+                        <label>Antwoord 1.....</label>
+                    </div>
+                    <div>
+                        <label>Vraag 2....</label></br>
+                        <label>Antwoord 2.....</label>
+                    </div>
+                    <div>
+                        <label>Vraag 3....</label></br>
+                        <label>Antwoord 3.....</label>
+                    </div>
+                </div>
+
+            </div>
         </div>
-
     </div>
-
-
-
-    <form method="POST" action="{{ route('contact.submit') }}">
-        @csrf
-        <div>
-            <label for="name">*Naam:</label>
-            <input type="text" id="name" name="name" required>
-        </div>
-        <div>
-            <label for="email">*Uw e-mailadres:</label>
-            <input type="email" id="email" name="email" required>
-        </div>
-        <div>
-            <label for="subject">Onderwerp:</label>
-            <input type="text" id="subject" name="subject">
-        </div>
-        <div>
-            <label for="message">Bericht:</label>
-            <textarea id="message" name="message"></textarea>
-        </div>
-        <div>
-            <button type="submit">Verstuur</button>
-        </div>
-    </form>
-
-    <div>
-         <h1>Contact gegevens</h1></br>
-         <label>Email</label></br>
-         <label>toneelverenigingchristina@gmail.com</label>
-     </div>
-     <div>
-         <label>Telefoonnummer<label></br>
-         <label>06-36176711<label>
-     </div>
-
-     <div>
-         <h1>FAQ</h1></br>
-         <label>Vraag 1....</label></br>
-         <label>Antwoord 1.....</label>
-     </div>
-     <div>
-         <label>Vraag 2....</label></br>
-         <label>Antwoord 2.....</label>
-     </div>
-     <div>
-         <label>Vraag 3....</label></br>
-         <label>Antwoord 3.....</label>
-     </div>
 </body>
+
 </html>
