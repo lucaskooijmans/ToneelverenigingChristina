38bead82f267cbb279c85bb1eb747126c5790e29
Thomas Versteeg <71022949+JathoSystems@users.noreply.github.com>
2024-03-14 10:42:41 +0100

    Contactformulier styling
    Voor nu nog alleen het formulier, kan nu niet verder omdat de contactinformatie nog mist.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,41 +1,58 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Contact</title>
 </head>
+
 <body>
     <x-navbar />
 
-    @if (session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
+    <div class="contact">
+
+        <div class="container">
+
+            @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
+
+            <h1>
+                Contact
+            </h1>
+
+            <form method="POST" action="{{ route('contact.submit') }}" class="contact-form">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Naam: <b>*</b></label>
+                    <input type="text" id="name" name="name" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Uw e-mailadres: <b>*</b></label>
+                    <input type="email" id="email" name="email" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="subject">Onderwerp:</label>
+                    <input type="text" id="subject" name="subject" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="message">Bericht:</label>
+                    <textarea id="message" name="message" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="button green-button">Verstuur</button>
+                </div>
+                <label><b>*</b> Verplicht veld</label>
+            </form>
+
+        </div>
+
     </div>
-    @endif
 
-
-    <form method="POST" action="{{ route('contact.submit') }}">
-        @csrf
-        <div>
-            <label for="name">*Naam:</label>
-            <input type="text" id="name" name="name" required>
-        </div>
-        <div>
-            <label for="email">*Uw e-mailadres:</label>
-            <input type="email" id="email" name="email" required>
-        </div>
-        <div>
-            <label for="subject">Onderwerp:</label>
-            <input type="text" id="subject" name="subject">
-        </div>
-        <div>
-            <label for="message">Bericht:</label>
-            <textarea id="message" name="message"></textarea>
-        </div>
-        <div>
-            <button type="submit">Verstuur</button>
-        </div>
-    </form>
 </body>
+
 </html>
