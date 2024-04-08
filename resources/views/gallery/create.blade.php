97aa9ade7fef83818ceadbf46607dbf3c363e0aa
Thomas Versteeg <71022949+JathoSystems@users.noreply.github.com>
2024-04-08 13:10:29 +0200

    Gallery tests

--- resources/views/gallery/create.blade.php
+++ resources/views/gallery/create.blade.php
@@ -21,16 +21,25 @@
             @csrf {{-- https://laravel.com/docs/10.x/csrf --}}
             <div class="form-group">
                 <label for="image">Afbeelding</label>
-                <input type="file" class="form-control" id="image" name="image" accept ="image/*" required >
+                <input type="file" class="form-control" id="image" name="image" accept ="image/*" >
+                @error('image')
+                    <p class="error">{{ $message }}</p>
+                @enderror
             </div>
 
             <div class="form-group">
                 <label for="title">Titel</label>
-                <input type="text" class="form-control" id="title" name="title" required>
+                <input type="text" class="form-control" id="title" name="title">
+                @error('title')
+                    <p class="error">{{ $message }}</p>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="description">Beschrijving</label>
-                <textarea class="form-control" id="description" name="description" rows="6" required></textarea>
+                <textarea class="form-control" id="description" name="description" rows="6"></textarea>
+                @error('description')
+                    <p class="error">{{ $message }}</p>
+                @enderror
             </div>
             <button type="submit" class="button green-button"><i class="fas fa-check"></i> Toevoegen</button>
         </form>
