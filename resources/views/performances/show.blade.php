ed2e70a453034f0a83c136bb8158613a2863e2b2
Delluci <wmn.brunenberg@student.avans.nl>
2024-04-22 15:45:31 +0200

    fix(Performances): admin auth error

--- resources/views/performances/show.blade.php
+++ resources/views/performances/show.blade.php
@@ -27,29 +27,30 @@
             </div>
         @endif
 
-        @if (auth()->user()->isAdmin())
-            <div class="admin-actions">
-                <h1 class="admin-heading">Eigenaar acties:</h1>
-                <form action="{{ route('performances.updateTicketAmount', $performance->id) }}" method="POST" class="admin-form">
-                    @csrf
-                    @method('PUT')
-                    <div class="form-group">
-                        <label for="ticket_change" class="form-label">Aantal tickets:</label>
-                        <input type="number" name="ticket_change" id="ticket_change" value="0" class="form-control" style="border: 1px solid black">
+        @auth
+            @if (auth()->user()->isAdmin())
+                <div class="admin-actions">
+                    <h1 class="admin-heading">Eigenaar acties:</h1>
+                    <form action="{{ route('performances.updateTicketAmount', $performance->id) }}" method="POST" class="admin-form">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group">
+                            <label for="ticket_change" class="form-label">Aantal tickets:</label>
+                            <input type="number" name="ticket_change" id="ticket_change" value="0" class="form-control" style="border: 1px solid black">
+                        </div>
+                        <div class="form-actions">
+                            <button type="submit" name="action" value="add" class="blue-button button">Toevoegen</button>
+                            <button type="submit" name="action" value="remove" class="blue-button button">Verwijderen</button>
+                        </div>
+                    </form>
+                    <div class="ticket-info">
+                        <p class="ticket-sold">Aantal tickets verkocht via de website: <span class="ticket-number">{{ $performance->tickets_sold }}</span></p>
+                        <p class="ticket-added">Aantal tickets toegevoegd: <span class="ticket-number">{{ $performance->tickets_added }}</span></p>
+                        <p class="ticket-removed">Aantal tickets verwijderd: <span class="ticket-number">{{ $performance->tickets_removed }}</span></p>
                     </div>
-                    <div class="form-actions">
-                        <button type="submit" name="action" value="add" class="blue-button button">Toevoegen</button>
-                        <button type="submit" name="action" value="remove" class="blue-button button">Verwijderen</button>
-                    </div>
-                </form>
-                <div class="ticket-info">
-                    <p class="ticket-sold">Aantal tickets verkocht via de website: <span class="ticket-number">{{ $performance->tickets_sold }}</span></p>
-                    <p class="ticket-added">Aantal tickets toegevoegd: <span class="ticket-number">{{ $performance->tickets_added }}</span></p>
-                    <p class="ticket-removed">Aantal tickets verwijderd: <span class="ticket-number">{{ $performance->tickets_removed }}</span></p>
                 </div>
-            </div>
-        @endif
-
+            @endif
+        @endauth
 
         <div class="container">
             <div class="split">
