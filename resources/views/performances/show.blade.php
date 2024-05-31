dc0300d263c76c2992f0276c34e756c6a15ce259
Thomas Versteeg <71022949+JathoSystems@users.noreply.github.com>
2024-05-31 10:24:15 +0200

    Stijl

--- resources/views/performances/show.blade.php
+++ resources/views/performances/show.blade.php
@@ -11,7 +11,8 @@
     <x-navbar />
 
     <div class="performances">
-        <div id="confirmationMessage" style="display: none; background-color: #4CAF50; color: white; padding: 10px; position: fixed; top: 0; left: 50%; transform: translateX(-50%); z-index: 9999;">
+        <div id="confirmationMessage"
+            style="display: none; background-color: #4CAF50; color: white; padding: 10px; position: fixed; top: 0; left: 50%; transform: translateX(-50%); z-index: 9999;">
             Bestelling succesvol geplaatst!
         </div>
 
@@ -32,7 +33,7 @@
                 {{ session('error') }}
             </div>
         @endif
-        
+
         @if (session('success'))
             <div class="alert alert-success">
                 {{ session('success') }}
@@ -41,42 +42,56 @@
 
         @auth
             @if (auth()->user()->isAdmin())
-                <a href="{{ route('tickets.exportTickets', $performance->id) }}" class="blue-button button">Exporteer verkochte tickets naar CSV</a>
+                <a href="{{ route('tickets.exportTickets', $performance->id) }}" class="blue-button button">Exporteer
+                    verkochte tickets naar CSV</a>
 
                 <div class="admin-actions">
                     <h1 class="admin-heading">Eigenaar acties:</h1>
-                    <form action="{{ route('tickets.updateTicketAmount', $performance->id) }}" method="POST" class="admin-form">
+                    <form action="{{ route('tickets.updateTicketAmount', $performance->id) }}" method="POST"
+                        class="admin-form">
                         @csrf
                         @method('PUT')
                         <div class="form-group">
                             <label for="ticket_change" class="form-label">Aantal tickets:</label>
-                            <input type="number" name="ticket_change" id="ticket_change" value="0" class="form-control" style="border: 1px solid black">
+                            <input type="number" name="ticket_change" id="ticket_change" value="0"
+                                class="form-control" style="border: 1px solid black">
                         </div>
                         <div class="form-actions">
-                            <button type="submit" name="action" value="add" class="blue-button button">Toevoegen</button>
-                            <button type="submit" name="action" value="remove" class="blue-button button">Verwijderen</button>
+                            <button type="submit" name="action" value="add"
+                                class="blue-button button">Toevoegen</button>
+                            <button type="submit" name="action" value="remove"
+                                class="blue-button button">Verwijderen</button>
                         </div>
                     </form>
                     <div class="ticket-info">
-                        <p class="ticket-removed">Huidig aantal beschikbare tickets: <span class="ticket-number">{{ $performance->tickets_remaining }}</span></p>
-                        <p class="ticket-added">Aantal tickets aangemaakt: <span class="ticket-number">{{ $performance->tickets_added }}</span></p>
-                        <p class="ticket-removed">Aantal tickets verwijderd: <span class="ticket-number">{{ $performance->tickets_removed }}</span></p>
-                        <p class="ticket-sold">Aantal tickets verkocht via de website: <span class="ticket-number">{{ $performance->tickets_sold }}</span></p>
+                        <p class="ticket-removed">Huidig aantal beschikbare tickets: <span
+                                class="ticket-number">{{ $performance->tickets_remaining }}</span></p>
+                        <p class="ticket-added">Aantal tickets aangemaakt: <span
+                                class="ticket-number">{{ $performance->tickets_added }}</span></p>
+                        <p class="ticket-removed">Aantal tickets verwijderd: <span
+                                class="ticket-number">{{ $performance->tickets_removed }}</span></p>
+                        <p class="ticket-sold">Aantal tickets verkocht via de website: <span
+                                class="ticket-number">{{ $performance->tickets_sold }}</span></p>
                     </div>
-                    <form action="{{ route('performances.updateSeatAmount', $performance->id) }}" method="POST" class="admin-form">
+                    <form action="{{ route('performances.updateSeatAmount', $performance->id) }}" method="POST"
+                        class="admin-form">
                         @csrf
                         @method('PUT')
                         <div class="form-group">
                             <label for="seats_change" class="form-label">Aantal stoelen:</label>
-                            <input type="number" name="seats_change" id="seats_change" value="0" class="form-control" style="border: 1px solid black">
+                            <input type="number" name="seats_change" id="seats_change" value="0" class="form-control"
+                                style="border: 1px solid black">
                         </div>
                         <div class="form-actions">
-                            <button type="submit" name="action" value="add" class="blue-button button">Toevoegen</button>
-                            <button type="submit" name="action" value="remove" class="blue-button button">Verwijderen</button>
+                            <button type="submit" name="action" value="add"
+                                class="blue-button button">Toevoegen</button>
+                            <button type="submit" name="action" value="remove"
+                                class="blue-button button">Verwijderen</button>
                         </div>
                     </form>
                     <div class="chair-info">
-                        <p class="available-seats">Huidig aantal stoelen: <span class="seats-number">{{ $performance->available_seats }}</span></p>
+                        <p class="available-seats">Huidig aantal stoelen: <span
+                                class="seats-number">{{ $performance->available_seats }}</span></p>
                     </div>
                 </div>
             @endif
@@ -92,7 +107,8 @@
                 <div class="checkout">
                     <h1>Bestel kaartjes</h1>
                     @if ($performance->tickets_remaining > 0)
-                        <form action="{{ route('payment.prepare', ['id' => $performance->id]) }}" method="POST" class="post-form">
+                        <form action="{{ route('payment.prepare', ['id' => $performance->id]) }}" method="POST"
+                            class="post-form">
                             @csrf
                             <div class="form-group">
                                 <label for="buyer_first_name">Voornaam</label>
@@ -109,7 +125,7 @@
                             <div class="form-group">
                                 <label for="amount">Aantal tickets</label>
                                 <input type="number" name="amount" required class="form-control">
-                            </div>  
+                            </div>
 
                             <button type="submit" class="blue-button button">Afrekenen</button>
                         </form>
@@ -118,19 +134,21 @@
                             <p>Helaas, er zijn geen tickets meer beschikbaar voor deze voorstelling.</p>
                         </div>
                     @endif
-                </div>              
-            </div>          
+                </div>
+            </div>
         </div>
     </div>
+    <div class="sponsors">
+        <div class="container"
+            style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
 
-    <div class="container" style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
-        <h1 style="margin: 0;">Locatie</h1>
-        <div style="width: 100%; height: 80%; display: flex; justify-content: center; align-items: center;">
-        <iframe src="https://storage.googleapis.com/maps-solutions-dmaxp2tpvp/locator-plus/dv31/locator-plus.html"
-                width="100%" height="100%"
-                style="border:0;"
-                loading="lazy">
-        </iframe>
+            <h1 style="margin: 0;">Locatie</h1>
+            <div style="width: 80vw; height: 80%; display: flex; justify-content: center; align-items: center;">
+                <iframe
+                    src="https://storage.googleapis.com/maps-solutions-dmaxp2tpvp/locator-plus/dv31/locator-plus.html"
+                    width="100%" height="100%" style="border:0;" loading="lazy">
+                </iframe>
+            </div>
         </div>
     </div>
 
