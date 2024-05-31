434ca0cba835c1e36f1922c5e88ce7747b885291
Thomas Versteeg <71022949+JathoSystems@users.noreply.github.com>
2024-05-31 10:34:29 +0200

    Dingetjes

--- resources/views/member/register.blade.php
+++ resources/views/member/register.blade.php
@@ -8,79 +8,99 @@
 </head>
 
 <body>
-    <x-navbar/>
+    <x-navbar />
+    <div class="sponsors">
 
-    <div class="register-form">
-        <h1>Inschrijven bij Toneelvereniging Christina</h1>
+        <div class="container">
+            <h1>Inschrijven bij Toneelvereniging Christina</h1>
 
-        @if(session('success'))
-            <p>{{ session('success') }}</p>
-        @endif
+            @if (session('success'))
+                <p>{{ session('success') }}</p>
+            @endif
 
-        @if(session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-        @endif
+            @if (session('error'))
+                <div class="alert alert-danger">
+                    {{ session('error') }}
+                </div>
+            @endif
 
-        @if($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
-        <form method="POST" action="{{ route('member.store') }}">
-            @csrf
+            <form method="POST" action="{{ route('member.store') }}" class="post-form">
+                @csrf
 
-            <label for="name">Naam:</label>
-            <input type="text" id="name" name="name" value="{{ old('name') }}" required>
-            <br>
+                <div class="form-group">
+                    <label for="name">Naam:</label>
+                    <input type="text" id="name" name="name" value="{{ old('name') }}" required
+                        class="form-control">
+                </div>
 
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" value="{{ old('email') }}" required>
-            <br>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" value="{{ old('email') }}" required
+                        class="form-control">
+                </div>
 
-            <label for="phoneNumber">Telefoonnummer:</label>
-            <input type="text" id="phoneNumber" name="phoneNumber" value="{{ old('phoneNumber') }}">
-            <br>
+                <div class="form-group">
+                    <label for="phoneNumber">Telefoonnummer:</label>
+                    <input type="text" id="phoneNumber" name="phoneNumber" value="{{ old('phoneNumber') }}"
+                        class="form-control">
+                </div>
 
-            <hr/>
+                <hr>
 
-            <label for="postalCode">Postcode:</label>
-            <input type="text" id="postalCode" name="postalCode" value="{{ old('postalCode') }}" required>
-            <br>
+                <div class="form-group">
+                    <label for="postalCode">Postcode:</label>
+                    <input type="text" id="postalCode" name="postalCode" value="{{ old('postalCode') }}" required
+                        class="form-control">
+                </div>
 
-            <label for="houseNumber">Huisnummer:</label>
-            <input type="text" id="houseNumber" name="houseNumber" value="{{ old('houseNumber') }}" required>
-            <br>
+                <div class="form-group">
+                    <label for="houseNumber">Huisnummer:</label>
+                    <input type="text" id="houseNumber" name="houseNumber" value="{{ old('houseNumber') }}" required
+                        class="form-control">
+                </div>
 
-            <label for="street">Straat:</label>
-            <input type="text" id="street" name="street" value="{{ old('street') }}" required>
-            <br>
+                <div class="form-group">
+                    <label for="street">Straat:</label>
+                    <input type="text" id="street" name="street" value="{{ old('street') }}" required
+                        class="form-control">
+                </div>
 
-            <label for="city">Plaats:</label>
-            <input type="text" id="city" name="city" value="{{ old('city') }}" required>
-            <br>
+                <div class="form-group">
+                    <label for="city">Plaats:</label>
+                    <input type="text" id="city" name="city" value="{{ old('city') }}" required
+                        class="form-control">
+                </div>
 
-            <hr/>
+                <hr />
 
-            <label for="pay">Ik wil nu betalen:</label>
-            <input type="checkbox" id="pay" name="pay">
-            <br>
+                <div class="form-group">
+                    <label for="pay">Ik wil nu betalen:</label>
+                    <input type="checkbox" id="pay" name="pay" class="form-control">
+                </div>
 
-            <hr/>
+                <hr />
 
-            <input type="checkbox" id="accept" name="accept" required>
-            <label for="accept">Ik ga akkoord met de <a href="/algemene_voorwaarden">algemene voorwaarden</a>.</label>
-            <br>
+                <div class="form-group">
+                    <label for="accept">Ik ga akkoord met de <a href="/algemene_voorwaarden">algemene
+                            voorwaarden</a>.</label>
+                    <input type="checkbox" id="accept" name="accept" required>
+                </div>
 
-            <button type="submit">Inschrijven</button>
-        </form>
+                <button type="submit">Inschrijven</button>
+            </form>
+        </div>
     </div>
+
 </body>
 
 </html>
